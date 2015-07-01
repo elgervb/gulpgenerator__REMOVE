a968116269427489965b55a1388e10ffebad01f9
elger <elgervb@gmail.com>
2015-07-01 22:15:57 +0200

    index page

--- gulpgenerator/index.php
+++ gulpgenerator/index.php
@@ -1,1 +1,8 @@
 <?php
+use compact\mvvm\FrontController;
+
+include_once '../compact/classes/compact/ClassLoader.php';
+compact\ClassLoader::create()->addClassPath(__DIR__ . '/app');
+
+$fc = new FrontController();
+$fc->run();
