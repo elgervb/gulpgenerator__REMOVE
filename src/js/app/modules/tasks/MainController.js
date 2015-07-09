da93b8929fa1db9d251df2b9952d7baeb7b96dd7
elger <elgervb@gmail.com>
2015-07-09 22:52:10 +0200

    #8 Add edit mode per task
    toggle edit mode and change title

--- src/js/app/modules/tasks/MainController.js
+++ src/js/app/modules/tasks/MainController.js
@@ -7,13 +7,16 @@ app.controller('TasksController', function($scope, TaskService) {
     $scope.tasks = response.data.task;
   });
 
+  $scope.scope = {};
+  $scope.scope.editmode = false;
 
   /**
    * Toggle a task and show the task body
    */
   $scope.toggle = function(task) {
-    if ($scope.toggled == task.name) {
+    if ($scope.toggled === task.name) {
       delete $scope.toggled;
+      delete $scope.scope.editmode;
     } else {
       $scope.toggled = task.name;
     }
