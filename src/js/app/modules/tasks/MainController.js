68a612c34e4160618fe982d974b0e01fe8efb902
elger <elgervb@gmail.com>
2015-07-09 22:18:20 +0200

    #10 angularify show / hide tasks with animations

--- src/js/app/modules/tasks/MainController.js
+++ src/js/app/modules/tasks/MainController.js
@@ -7,4 +7,17 @@ app.controller('TasksController', function($scope, TaskService) {
     $scope.tasks = response.data.task;
   });
 
+
+  /**
+   * Toggle a task and show the task body
+   */
+  $scope.toggle = function(task) {
+    if ($scope.toggled == task.name) {
+      delete $scope.toggled;
+    } else {
+      $scope.toggled = task.name;
+    }
+    
+  };
+
 });
