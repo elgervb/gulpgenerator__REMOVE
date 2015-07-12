7b5fd647ecc1145f249baf45ceea3069eb4fd9c5
elger <elgervb@gmail.com>
2015-07-12 20:19:22 +0200

    added comments

--- src/js/app/modules/new/NewGulpfileController.js
+++ src/js/app/modules/new/NewGulpfileController.js
@@ -1,8 +1,15 @@
 
+/**
+ * Controller to help the user create a new gulp file. This page let's the user enter the 
+ * package.json details.
+ */
 app.controller('NewGulpfileController', function($scope, $location, SharedData) {
 
   $scope.package = SharedData.package;
 
+  /**
+   * When the user entered a name for the gulpfile, then the right cached version will be loaded
+   */
   $scope.$watch('package.name', function(newValue, oldValue) {
     if (newValue && newValue !== oldValue) {
       var cache = SharedData.load($scope.package.name);
@@ -12,6 +19,9 @@ app.controller('NewGulpfileController', function($scope, $location, SharedData) 
     }
   });
 
+  /**
+   * Continue to the next page: generator where the user can add, edit or delete tasks
+   */
   $scope.continue = function() {
     SharedData.store();
 
