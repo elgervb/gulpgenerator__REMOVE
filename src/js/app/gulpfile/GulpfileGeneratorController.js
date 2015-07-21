71e34514bca081be0172fb5d31faf24f99c59cce
Elger van Boxtel <elgervb@gmail.com>
2015-07-21 10:22:15 +0200

    serve gulpfile for download
    #1

--- src/js/app/gulpfile/GulpfileGeneratorController.js
+++ src/js/app/gulpfile/GulpfileGeneratorController.js
@@ -1,8 +1,16 @@
 /**
  * Main controller
  */
-app.controller('GulpfileGeneratorController', function($scope, $routeParams, TaskService, SharedData) {
+app.controller('GulpfileGeneratorController', function($scope, $routeParams, $http, TaskService, SharedData, BaseUrl) {
 
   $scope.package = SharedData.load($routeParams.guid);
 
+  $scope.download = function(){
+    //$http.get(BaseUrl + 'generate/' + $routeParams.guid);
+    var iframe = document.createElement('iframe');
+    iframe.classList.add('download-frame');
+    iframe.src = BaseUrl + 'generate/' + $routeParams.guid;
+    iframe.style.display = "none";
+    document.body.appendChild(iframe);  
+  }
 });
